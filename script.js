1a31038ed3855a267bc23f00e0b723c1145baf20
sxngmadara <sxngmadara@gmail.com>
2024-09-26 19:37:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-// See https://www.npmjs.com/package/sweetalert2 documentation.
-import * as sweetalert2 from "https://esm.run/sweetalert2"
-
 console.log(sweetalert2)
-const form = document.getElementById("transactionForm");
+const form = document.getElementById("http://127.0.0.1:5500/index.html");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const formData = new FormData(form);
-
-    fetch(form.action, {
-        method: "POST",
-        body: formData,
-        _mode: "no-cors",
-    }).then(response => {
-        alert("success!");
-    }).catch(error => {
-        alert("Failed to submit transaction!");
-        console.error("Error:", error);
-    });
+form.addEventListener("submit", function stop(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return e
 });
